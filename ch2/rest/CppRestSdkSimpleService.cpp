f59f07c2cfab14caa33b05d1f7d5b93c3a363ed0
Adrian Ostrowski <adr.ostrowski@gmail.com>
2019-10-27 23:20:41 +0100

    CH2: Fix code style

--- ch2/rest/CppRestSdkSimpleService.cpp
+++ ch2/rest/CppRestSdkSimpleService.cpp
@@ -4,30 +4,37 @@
 
 #include <thread>
 
-void respond(const http_request &request, const status_code &status, const json::value &response) {
-    json::value resp;
-    resp[U("status")] = json::value::number(status);
-    resp[U("response")] = response;
-    request.reply(status, resp);
+using namespace ::web;
+using namespace ::web::http;
+using namespace std::literals;
+
+void respond(const http_request &request, const status_code &status,
+             const json::value &response) {
+  json::value resp;
+  resp[U("status")] = json::value::number(status);
+  resp[U("response")] = response;
+  request.reply(status, resp);
 }
 
 int main() {
-    auto listener = http_listener(U("http://localhost/greeter"));
-    listener.open().wait();
-    listener.support(methods::GET, [](http_request req) {
-        auto parameters = uri::split_query(req.request_uri().query());
-        auto keyValueIt = parameters.find(U("name"));
+  auto listener = ::web::http::experimental::listener::http_listener(
+      U("http://localhost/greeter"));
+  listener.open().wait();
+  listener.support(methods::GET, [](http_request req) {
+    auto parameters = uri::split_query(req.request_uri().query());
+    auto keyValueIt = parameters.find(U("name"));
 
-        if (keyValueIt == end(parameters)) {
-            auto err = U("Missing value for 'name'");
-            respond(req, status_codes::BadRequest, json::value::string(err));
-            return;
-        }
+    if (keyValueIt == end(parameters)) {
+      auto err = U("Missing value for 'name'");
+      respond(req, status_codes::BadRequest, json::value::string(err));
+      return;
+    }
 
-        auto name = keyValueIt->second;
-        respond(req, status_codes::OK, json::value::string(U("Hello ") + name + "!"));
-    });
+    auto name = keyValueIt->second;
+    respond(req, status_codes::OK,
+            json::value::string(U("Hello ") + name + "!"));
+  });
 
-    std::thread::sleep_for(60s);
-    listener.close();
+  std::this_thread::sleep_for(60s);
+  listener.close();
 }
