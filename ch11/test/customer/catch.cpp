d0580fa3ef6025804af7b3c5c2e994ded9336e51
Adrian Ostrowski <adr.ostrowski@gmail.com>
2019-11-30 15:19:11 +0100

    Fix Catch2 unit test

--- ch11/test/customer/catch.cpp
+++ ch11/test/customer/catch.cpp
@@ -1,7 +1,7 @@
 #include "customer/customer.h"
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include "catch2/catch.hpp"
 
 TEST_CASE( "Basic responses", "Given Name When Prepare Reponses Then Greets Friendly" ) {
   auto name = "Bob";
