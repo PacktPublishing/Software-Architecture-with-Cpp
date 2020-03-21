70ebe082f2bd5d63829be5b4641c05631ba2835b
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2020-03-21 11:54:29 +0100

    Writing testable code

---
+++ ch10/test/customer/catch.cpp
@@ -0,0 +1,11 @@
+#include "customer/customer.h"
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+TEST_CASE( "Basic responses", "Given Name When Prepare Reponses Then Greets Friendly" ) {
+  auto name = "Bob";
+  auto code_and_string = prepare_response(name);
+  REQUIRE(code_and_string.first == web::http::status_codes::OK);
+  REQUIRE(code_and_string.second == web::json::value("Hello, Bob!"));
+}
