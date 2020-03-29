18ed511b60da44b859f3e0259b85dcbd46543b9c
Adrian Ostrowski <adr.ostrowski@gmail.com>
2020-03-29 16:22:05 +0200

    CH11: Fix CppUnit issues
    It will now be checked out correctly and build only when needed.

---
+++ ch11/cmake/FetchCppUnit.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+
+FetchContent_Declare(
+        cppunit
+        GIT_REPOSITORY https://github.com/aostrowski/CppUnit.git
+        GIT_TAG master
+        GIT_SHALLOW ON
+)
+FetchContent_MakeAvailable(cppunit)
+message(STATUS "CppUnit fetched successfully")
