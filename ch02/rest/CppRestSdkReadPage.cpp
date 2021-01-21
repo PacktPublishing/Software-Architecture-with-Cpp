3b5c0dd9c720c21a73cdaeeed5a192fb03bdfb8c
Andrey Gavrilin <gaal.dev@gmail.com>
2021-01-21 11:10:53 +0300

    fix chapter 02

--- ch02/rest/CppRestSdkReadPage.cpp
+++ ch02/rest/CppRestSdkReadPage.cpp
@@ -5,6 +5,11 @@
 #include <memory>
 #include <ostream>
 
+using namespace concurrency::streams;
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+
 int main() {
   auto fileStream = std::make_unique<ostream>();
 
@@ -13,12 +18,15 @@ int main() {
           .then([&fileStream](ostream fout) {
             *fileStream = fout;
 
-            auto client = http_client(U("http://localhost"));
+            auto client = http_client(U("http://localhost:3000"));
             auto builder = uri_builder(U("/greeter"));
             builder.append_query(U("name"), U("World"));
             return client.request(methods::GET, builder.to_string());
           })
           .then([&fileStream](http_response response) {
+            std::cout << "response:" << std::endl
+                      << response.to_string() << std::endl;
+
             // write the response to file
             response.body().read_to_end(fileStream->streambuf());
           });
@@ -26,5 +34,6 @@ int main() {
     httpRequestTask.wait();
   } catch (const std::exception &e) {
     // handle the exception
+    std::cerr << e.what();
   }
 }
