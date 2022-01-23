58aa5595eb63cef474faa51c5b700a6d43e37cc9
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2022-01-23 13:48:22 +0100

    Make lsb-release optional on macOS

--- Chapter14/default.nix
+++ Chapter14/default.nix
@@ -1,65 +1,68 @@
 { pkgs ? import (
   builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/archive/f460e62d9b367a0bfd71148ef34f44421d0e82cb.tar.gz";
-    sha256 = "1pazl2zy39zjih6s1dyvlxm849mwj3f6ijji2c3913n6kw0s3nh0";
+    url = "https://github.com/NixOS/nixpkgs/archive/21.11-pre.tar.gz";
+    sha256 = "0ww19c7n4fj9zn770aw8zaqld742bi9sa9s8hqb3vrgp3mpihil0";
   }
 ) {} }:
 
-with pkgs;
+let
+  additionalInputs = if pkgs.system == "x86_64-linux" then pkgs.lsb-release else "";
+in
+with pkgs; {
+  gcc11Env = stdenvNoCC.mkDerivation {
+    name = "gcc11-environment";
+    buildInputs = [
+      ansible
+      autoconf
+      automake
+      buildah
+      conmon
+      clang
+      cmake
+      docker
+      ensureNewerSourcesForZipFilesHook
+      gcc
+      libtool
+      lsb-release
+      nomad
+      pkg-config
+      podman
+      pre-commit
+      runc
+      python3Packages.setuptools
+      python3Packages.pip
+      python3Packages.virtualenv
+      slirp4netns
+    ];
+    shellHook = ''
+      # <setup for buildah toolchain>
+      sudo mkdir -p /etc/containers
+      cat <<EOF | sudo tee /etc/containers/policy.json
+      {
+          "default": [
+              {
+                  "type": "insecureAcceptAnything"
+              }
+          ]
+      }
+      EOF
+      cat <<EOF | sudo tee /etc/containers/registries.conf
+      [registries.search]
+      registries = [ 'docker.io' ]
+      EOF
 
-mkShell {
-  buildInputs = [
-    ansible
-    autoconf
-    automake
-    buildah
-    conmon
-    clang
-    cmake
-    docker
-    ensureNewerSourcesForZipFilesHook
-    gcc
-    libtool
-    lsb-release
-    nomad
-    pkg-config
-    podman
-    pre-commit
-    runc
-    python3Packages.setuptools
-    python3Packages.pip
-    python3Packages.virtualenv
-    slirp4netns
-  ];
-  shellHook = ''
-    pre-commit install -f --hook-type pre-commit
-    # <setup for buildah toolchain>
-    sudo mkdir -p /etc/containers
-    cat <<EOF | sudo tee /etc/containers/policy.json
-    {
-        "default": [
-            {
-                "type": "insecureAcceptAnything"
-            }
-        ]
-    }
-    EOF
-    cat <<EOF | sudo tee /etc/containers/registries.conf
-    [registries.search]
-    registries = [ 'docker.io' ]
-    EOF
-
-    # documentation for this is very disorganized at this point
-    # see https://github.com/containers/libpod/blob/master/docs/libpod.conf.5.md
-    cat <<EOF | tee $HOME/podman.conf
-    conmon_path = [ "$(which conmon)" ]
-    events_logger = "file"
-    [runtimes]
-    runc = [ "$(which runc)" ]
-    EOF
-    unset SOURCE_DATE_EPOCH
-    virtualenv venv
-    source venv/bin/activate
-    pip install ansible-bender==0.9.0 docker-compose==1.25.5
-  '';
+      # documentation for this is very disorganized at this point
+      # see https://github.com/containers/libpod/blob/master/docs/libpod.conf.5.md
+      cat <<EOF | tee $HOME/podman.conf
+      conmon_path = [ "$(which conmon)" ]
+      events_logger = "file"
+      [runtimes]
+      runc = [ "$(which runc)" ]
+      EOF
+      unset SOURCE_DATE_EPOCH
+      virtualenv venv
+      source venv/bin/activate
+      pip install ansible-bender==0.9.0 docker-compose==1.25.5
+    '';
+  };
 }
--- default.nix
+++ default.nix
@@ -1,34 +1,33 @@
 { pkgs ? import (
   builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/a439e0ef6eb2ab395048b3cf68a46224874245ef.tar.gz";
-    sha256 = "005z8j5bikrlr2rxh0i3znygkdp8y6b9cbshm986n2mrf47zpbri";
+    url = "https://github.com/NixOS/nixpkgs/archive/21.11-pre.tar.gz";
+    sha256 = "0ww19c7n4fj9zn770aw8zaqld742bi9sa9s8hqb3vrgp3mpihil0";
   }
 ) {} }:
 
-with pkgs;
-
-mkShell {
-  buildInputs = [
-    autoconf
-    automake
-    clang
-    cmake
-    docker
-    docker-compose
-    doxygen
-    gcc
-    libtool
-    lsb-release
-    pkg-config
-    pre-commit
-    python3Packages.setuptools
-    python3Packages.pip
-    python3Packages.virtualenv
-  ];
-  shellHook = ''
-    pre-commit install -f --hook-type pre-commit
-    virtualenv venv
-    source venv/bin/activate
-    pip install conan==1.34.1 sphinx sphinx-rtd-theme breathe m2r2
-  '';
+let
+  additionalInputs = if pkgs.system == "x86_64-linux" then pkgs.lsb-release else "";
+in
+with pkgs; {
+  gcc11Env = stdenvNoCC.mkDerivation {
+    name = "gcc11-environment";
+    buildInputs = [
+      autoconf
+      automake
+      clang
+      cmake
+      conan
+      docker
+      docker-compose
+      doxygen
+      gcc11
+      libtool
+      pkg-config
+      pre-commit
+      additionalInputs
+    ];
+    shellHook = ''
+      pre-commit install -f --hook-type pre-commit
+    '';
+  };
 }
